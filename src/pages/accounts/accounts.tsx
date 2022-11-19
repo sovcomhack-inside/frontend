b7df31c3df78781842e1bc908491eb6e0834a1b6
senago <mirzoyan.oganes@mail.ru>
2022-11-19 23:24:05 +0300

    [pages] Improve accounts design

--- src/pages/accounts/accounts.tsx
+++ src/pages/accounts/accounts.tsx
@@ -15,18 +15,18 @@ export const AccountsPage: React.FC = () => {
 
   const mapAccount = (account: Account) => {
     return <div className='flex flex-row justify-between place-items-center rounded-md p-6 bg-neutral-900'>
-      <div className='flex flex-col justify-between'>
-        <div className='class'>{account.currency_name}</div>
-        <div className='text-neutral-400 text-xss'>Cоздан: {account.created_at}</div>
+      <div className='flex flex-col gap-1'>
+        <div className='text-lg font-bold'>{account.currency_name}</div>
+        <div className='text-xss text-neutral-400'>Cоздан: {account.created_at}</div>
       </div>
-      <div className=''>
-        {account.currency_name}
+      <div className='rounded-full w-10 grid place-items-center aspect-square bg-white text-black'>
+        $
       </div>
     </div>
   }
 
   return (
-    <div className='flex flex-col w-80 mx-auto'>
+    <div className='flex flex-col w-80 mx-auto gap-4'>
       <BackButton onClick={() => navigate("/profile")} />
       <div className='text-4xl font-bold'>Мои счета</div>
       <div className='flex flex-col gap-2 h-1/4 overflow-hidden'>
@@ -34,7 +34,7 @@ export const AccountsPage: React.FC = () => {
       </div>
       <Link
         to={{ pathname: '/accounts/new' }}
-        className='bg-neutral-900 p-6 rounded-sm text-center'
+        className='text-lg bg-neutral-900 p-6 rounded-md text-center hover:bg-neutral-800'
       >
         Новый валютный счет
       </Link>
